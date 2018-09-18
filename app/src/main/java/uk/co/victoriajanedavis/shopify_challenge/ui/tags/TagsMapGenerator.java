9cc0f8051e99ab6e5e7212447c8d412734306659
sbearben <armonk@gmail.com>
2018-09-18 02:04:01 -0400

    Changed to use TreeMap on the Tags screen so that the items are sorted.

--- app/src/main/java/uk/co/victoriajanedavis/shopify_challenge/ui/tags/TagsMapGenerator.java
+++ app/src/main/java/uk/co/victoriajanedavis/shopify_challenge/ui/tags/TagsMapGenerator.java
@@ -1,10 +1,16 @@
 package uk.co.victoriajanedavis.shopify_challenge.ui.tags;
 
+import android.support.annotation.NonNull;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.SortedMap;
+import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 import uk.co.victoriajanedavis.shopify_challenge.data.model.Product;
@@ -17,7 +23,7 @@ public class TagsMapGenerator {
 
     public static Map<String, List<Product>> generateTagsMapFromProductsList(List<Product> productsList) {
         mProductsList = productsList;
-        mTagsMap = new ConcurrentHashMap<>();
+        mTagsMap = new TreeMap<>();
         return generateTagsMap();
     }
 
